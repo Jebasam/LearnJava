db06c26d369c294af3617fe8dde57dc83c6d3ce8
Jebasam <jebasam.official@gmail.com>
2021-09-15 18:21:11 +0530

    Update literals.java

--- Learn Java/src/data_types/literals.java
+++ Learn Java/src/data_types/literals.java
@@ -12,6 +12,31 @@ public class literals {
 		//Binary Literals
 		int bina = 0b1101_0101_0001_1010;
 		System.out.println(bina);
+		//FLOATING POINT LITERALS
+		double num = 9_433_5_54.0;
+		double num1 = 94_5_287_6.43_65_3;
+		System.out.println(num);
+		System.out.println(num1);
+		
+		//Boolean Literals
+		/*
+		 * 1 or 0 are not compiled as boolean value 
+		 * It is Integer value
+		 * 
+		 * int a=0;
+		 * while(a){
+		 * System.out.println("Hello");
+		 * }
+		 * 
+		 * The above program will be a compile time error as while cannot take 1 as 
+		 * boolean value.
+		 * */
+		//CHARACTER LITERALS
+		char s = '\141';//Octal notation for 'a'. '\'followed by number
+		System.out.println(s);
+		char rr = '\u0062';//Hexadecimal notation for 'b'. '\' followed by 'u' and number.
+		System.out.println(rr);
+		System.out.println("Hai get\"s");
 	}
 
 }
